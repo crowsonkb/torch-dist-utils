349ffe51288e7e2dafb9a795b791fc26352f22cd
Katherine Crowson <crowsonkb@gmail.com>
2023-09-22 10:28:34 +0000

    Add links to source code in docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
+import importlib
+import inspect
+from pathlib import Path
 import sys
 
-sys.path.insert(0, os.path.abspath("../"))
+base_path = Path(__file__).parent.parent.resolve()
+sys.path.insert(0, str(base_path))
 
 
 # -- Project information -----------------------------------------------------
@@ -21,6 +24,7 @@ author = "Katherine Crowson"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.linkcode",
     "sphinx_rtd_theme",
 ]
 
@@ -34,7 +38,43 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 pygments_style = "sphinx"
 
-# -- Options for autodoc -----------------------------------------------------
+# -- Options for autodoc extension -------------------------------------------
 
 autodoc_member_order = "bysource"
 autodoc_typehints = "description"
+
+# -- Options for linkcode extension ------------------------------------------
+
+code_url = "https://github.com/crowsonkb/dist-utils/blob/main"
+
+
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+
+    mod = importlib.import_module(info["module"])
+    if "." in info["fullname"]:
+        objname, attrname = info["fullname"].split(".")
+        obj = getattr(mod, objname)
+        try:
+            # object is a method of a class
+            obj = getattr(obj, attrname)
+        except AttributeError:
+            # object is an attribute of a class
+            return None
+    else:
+        obj = getattr(mod, info["fullname"])
+
+    try:
+        file = inspect.getsourcefile(obj)
+        lines = inspect.getsourcelines(obj)
+    except TypeError:
+        # e.g. object is a typing.Union
+        return None
+    try:
+        file = Path(file).relative_to(base_path)
+    except ValueError:
+        return None
+    start, end = lines[1], lines[1] + len(lines[0]) - 1
+
+    return f"{code_url}/{file}#L{start}-L{end}"
