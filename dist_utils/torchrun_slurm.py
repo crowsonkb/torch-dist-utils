e06072f51abd4d494dcf7f22a4ff3a328d9705cd
Katherine Crowson <crowsonkb@gmail.com>
2023-09-22 06:49:05 +0000

    Print a more informative error if torchrun_slurm is not run within a Slurm job

--- dist_utils/torchrun_slurm.py
+++ dist_utils/torchrun_slurm.py
@@ -8,17 +8,29 @@ import sys
 
 
 def main():
-    proc = Popen(["scontrol", "show", "hostnames", os.environ["SLURM_JOB_NODELIST"]], stdout=PIPE)
+    try:
+        nnodes = os.environ["SLURM_NPROCS"]
+        node_rank = os.environ["SLURM_PROCID"]
+        nodelist = os.environ["SLURM_JOB_NODELIST"]
+    except KeyError:
+        print(
+            "This script must be run within a Slurm job. The environment variables "
+            "SLURM_NPROCS, SLURM_PROCID, and SLURM_JOB_NODELIST were not found.",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    proc = Popen(["scontrol", "show", "hostnames", nodelist], stdout=PIPE)
     master_addr = proc.stdout.read().decode().splitlines()[0]
 
     command = [
         "torchrun",
         "--nnodes",
-        os.environ["SLURM_NPROCS"],
+        nnodes,
         "--nproc-per-node",
         "gpu",
         "--node_rank",
-        os.environ["SLURM_PROCID"],
+        node_rank,
         "--master_addr",
         master_addr,
         *sys.argv[1:],
